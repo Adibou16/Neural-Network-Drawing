8ef95a4952c9d9a48eb45200ea75aa3ac137e8a5
Felix <filabert16@outlook.con>
2020-06-05 11:23:56 -0400

    Simplification
    J'ai un cerveau mtn BTW YA TOUJOURS L'ERREUR DE LISTENER EVENT JAR WTF

--- Neural Network/draw.js
+++ Neural Network/draw.js
@@ -1,5 +1,5 @@
 const canvas = document.querySelector("canvas");
-const clearButton = document.getElementById("clear");
+const clearButton = document.querySelector("clear");
 const ctx = canvas.getContext("2d");
 const scale = 10;
 const arraySize = 28;
@@ -13,6 +13,9 @@ var y;
 var array = new Array(arraySize);
 
 (function setup() {
+    array = [];
+    ctx.clearRect(scale, scale, scale, scale);
+
     for (var i = 0; i < arraySize; i++) {
         array[i] = new Array(arraySize);
     }
@@ -22,9 +25,9 @@ function draw() {
     for (let i = 0; i < array.length; i++) {
         for (let j = 0; j < array[i].length; j++) {
             ctx.beginPath();
-            ctx.rect(i * scale, j * scale, scale, scale)
+            ctx.rect(i * scale, j * scale, scale, scale);
             ctx.fillStyle = array[i][j] ? "white" : "gray";
-            ctx.fill()
+            ctx.fill();
         }
     }
     requestAnimationFrame(draw);
@@ -32,32 +35,28 @@ function draw() {
 requestAnimationFrame(draw);
 
 function pen(event) {
-    let x = Math.floor(event.clientX / scale)
-    let y = Math.floor(event.clientY / scale)
+    let x = Math.floor(event.clientX / scale);
+    let y = Math.floor(event.clientY / scale);
 
-    array[x][y] = 1
+    array[x][y] = 1;
 }
 
 let clicked = false
 canvas.addEventListener("mousedown", (e) => {
-    clicked = true
-    pen(e)
+    clicked = true;
+    pen(e);
 });
 
 canvas.addEventListener("mouseup", () => {
-    clicked = false
+    clicked = false;
 });
 
 canvas.addEventListener("mousemove", (e) => {
-    if (clicked) pen(e)
+    if (clicked) pen(e);
 });
 
 canvas.addEventListener("mouseleave", () => {
-    clicked = false
+    clicked = false;
 });
 
-clearButton.addEventListener("click", () => {
-    array = [];
-    setup();
-    ctx.clearRect(scale, scale, scale, scale);
-});
+clearButton.addEventListener("click", setup);
